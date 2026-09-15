03baf3d15ac1c388cf814b8b2a94e35f7985e73a
RicardoAD17 <ricardoalmada036@gmail.com>
2026-09-15 09:27:01 -0600

    .

--- tacocloud/tacocloud-api/src/main/java/tacos/web/api/OrderApiController.java
+++ tacocloud/tacocloud-api/src/main/java/tacos/web/api/OrderApiController.java
@@ -2,7 +2,6 @@ package tacos.web.api;
 
 import javax.validation.Valid;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,10 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import tacos.TacoOrder;
